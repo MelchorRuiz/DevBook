e2314739400f1e5ea6f6588a0d316f8d5a445610
MelchorRuiz <ruizgonzalezmelchor3@gmail.com>
2024-12-30 18:17:17 +0100

    Create the initial design

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className='bg-slate-800 w-screen h-screen flex flex-col font-roboto text-white'>
+      <header className="flex p-4 bg-slate-900/20">
+        <p className='text-2xl font-bold cursor-pointer hover:rotate-3 hover:scale-105'>
+          DEV BOOK
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </header>
+      <main className="flex justify-center items-center flex-1">
+        <p className="text-xl">Proximamente...</p>
+      </main>
+    </div>
   )
 }
 
