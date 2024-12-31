92a2725e012f0b835d966e84e870799a3084e1ac
MelchorRuiz <ruizgonzalezmelchor3@gmail.com>
2024-12-31 02:24:36 +0100

    Revamp Home component layout and content for enhanced user engagement

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,23 @@
+import { Link } from 'react-router-dom';
+
 export const Home = () => {
     return (
-        <div className="bg-slate-900/30 flex-1 rounded-lg p-4 overflow-y-scroll">
-            <h2 className='text-4xl'>Bienvenido a DEV BOOK</h2>
-            <p className='text-lg pt-2'>Crear y editar documentos de desarrollo nunca fue tan fácil.</p>
+        <div className="bg-slate-900/30 flex-1 rounded-lg p-4 flex flex-col pt-20 items-center gap-10">
+            <h1 className="text-8xl font-bold text-center">Diviertete estudiando programación</h1>
+            <h3 className="text-2xl text-white/60 text-center">Este proyecto es una aplicación web diseñada para ayudar a los programadores a tomar notas de manera interactiva. Permite a los usuarios crear documentos de notas, agregar ejercicios y código.</h3>
+            <div className='flex gap-4 items-center'>
+                <Link to="/documentos">
+                    <button className="p-3 text-xl text-black bg-amber-400/90 rounded-lg hover:bg-amber-400/70 hover:scale-105 hover:rotate-3">
+                        Empezar
+                    </button>
+                </Link>
+                <a 
+                    className="text-lg"
+                    href="https://github.com/MelchorRuiz/DevBook"
+                >
+                    Código fuente &rarr;
+                </a>
+            </div>
         </div>
     )
 }
